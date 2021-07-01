286ba0898c2b87e2699ce010bbb6dfebfb0aabb9
louknows <louknowsalready@gmail.com>
2021-07-01 18:12:14 +0800

    completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
 
+	// Change Theme Events
+	$(document).on('click','#theme1',function(){
+		$('.toggle-btn > li.active').removeClass('active');
+		$(this).addClass('active');
+
+		$('.theme-main-bg').removeClass('theme1 theme2 theme3').addClass('theme1');
+		$('.toggle-btn').removeClass('theme1 theme2 theme3').addClass('theme1');
+		$('.screen').removeClass('theme1 theme2 theme3').addClass('theme1');
+		$('#total').removeClass('theme1 theme2 theme3').addClass('theme1');
+		$('.key-grp').removeClass('theme1 theme2 theme3').addClass('theme1');
+		$('.num-op-key').removeClass('theme1 theme2 theme3').addClass('theme1');
+		$('.special-key').removeClass('theme1 theme2 theme3').addClass('theme1');
+		$('.eql-key').removeClass('theme1 theme2 theme3').addClass('theme1');
+	});
+
+	$(document).on('click','#theme2',function(){
+		$('.toggle-btn > li.active').removeClass('active');
+		$(this).addClass('active');
+
+		$('.theme-main-bg').removeClass('theme1 theme2 theme3').addClass('theme2');
+		$('.toggle-btn').removeClass('theme1 theme2 theme3').addClass('theme2');
+		$('.screen').removeClass('theme1 theme2 theme3').addClass('theme2');
+		$('#total').removeClass('theme1 theme2 theme3').addClass('theme2');
+		$('.key-grp').removeClass('theme1 theme2 theme3').addClass('theme2');
+		$('.num-op-key').removeClass('theme1 theme2 theme3').addClass('theme2');
+		$('.special-key').removeClass('theme1 theme2 theme3').addClass('theme2');
+		$('.eql-key').removeClass('theme1 theme2 theme3').addClass('theme2');
+	});
+
+
+	$(document).on('click','#theme3',function(){
+		$('.toggle-btn > li.active').removeClass('active');
+		$(this).addClass('active');
+
+		$('.theme-main-bg').removeClass('theme1 theme2 theme3').addClass('theme3');
+		$('.toggle-btn').removeClass('theme1 theme2 theme3').addClass('theme3');
+		$('.screen').removeClass('theme1 theme2 theme3').addClass('theme3');
+		$('#total').removeClass('theme1 theme2 theme3').addClass('theme3');
+		$('.key-grp').removeClass('theme1 theme2 theme3').addClass('theme3');
+		$('.num-op-key').removeClass('theme1 theme2 theme3').addClass('theme3');
+		$('.special-key').removeClass('theme1 theme2 theme3').addClass('theme3');
+		$('.eql-key').removeClass('theme1 theme2 theme3').addClass('theme3');
+	});
+
+	// Calculator Events
+
 	$(document).on('click',".num",function(){
 		if($('#formula').css('display') == 'none')
 			reset();
@@ -18,20 +64,12 @@ $(document).ready(function(){
 		let formula = $('#formula').text().trim();
 		let opkey = $(this).data('value');
 
-		if(formula.length > 0){
-			let last_char = formula.slice(-1);
-
-			if('+-×/'.includes(last_char) && formula.length > 1){
-				formula = formula.slice(0,-1) + opkey;
-				$('#formula').text(formula);
-			}else if('1234567890'.includes(last_char)){
-				$('#formula').append(opkey);
-			}
-		}else{
-			if(opkey == '-')
-				$('#formula').append(opkey);
+		if(isDoubleOperator(formula, opkey)){
+			formula = formula.slice(0,-1) + opkey;
+			$('#formula').text(formula);
+		}else if(isValidOperator(formula, opkey)){
+			$('#formula').append(opkey);
 		}
-
 	});
 
 	$(document).on('click','#key-eql', function(){
@@ -39,17 +77,9 @@ $(document).ready(function(){
 
 		executeCalc(statement);
 		if($('#total').text().trim().length){
-			let theme = $('.toggle-btn > .active').data('theme');
 			$('.screen').css({'justify-content': 'center','font-size' : '2rem'});
 			$('#formula').css({'display': 'none'});
-			switch(theme){
-				case 1:
-					$('#total').css({'color': 'var(--theme1-total-active)'}); break;
-				case 2:
-					$('#total').css({'color': 'var(--theme2-total-active)'}); break;
-				case 3:
-					$('#total').css({'color': 'var(--theme3-total-active)'}); break;	
-			}
+			$('#total').addClass('eql');
 		}
 	});
 
@@ -59,11 +89,25 @@ $(document).ready(function(){
 			reset();
 		
 		let dotkey = $(this).data('value');
-		$('#formula').append(dotkey);
+		let formula = $('#formula').text().trim();
+		if(isValidDot(formula))
+			$('#formula').append(dotkey);
 	});
 
 	$(document).on('click',"#key-res",function(){
 		reset();
+	});
+
+	$(document).on('click','#key-del',function(){
+		let formula = $('#formula').text().trim();
+		formula = formula.slice(0,-1);
+
+		$('#formula').text(formula);
+
+		if(isValidGeneralPattern(formula))
+			executeCalc(formula);
+		else
+			$('#total').text('');
 	});
 
 
@@ -82,30 +126,28 @@ $(document).ready(function(){
 		statement = statement.replace(/^[\+\/×]|[\+\-\/×\.]$/,'');
 
 		if(isValidGeneralPattern(statement)){
-			if(isDivideZeroNotExist()){
-				let higher_pattern = /^-?(?:\.|(?:\d+\.))?\d+[×\/](?:\.|(?:\d+\.))?\d+|(?:\.|(?:\d+\.))?\d+[×\/](?:\.|(?:\d+\.))?\d+/
-				let lower_pattern = /^-?(?:\.|(?:\d+\.))?\d+[-\+](?:\.|(?:\d+\.))?\d+/
+			let higher_pattern = /^-?(?:\.|(?:\d+\.))?\d+[×\/](?:\.|(?:\d+\.))?\d+|(?:\.|(?:\d+\.))?\d+[×\/](?:\.|(?:\d+\.))?\d+/
+			let lower_pattern = /^-?(?:\.|(?:\d+\.))?\d+[-\+](?:\.|(?:\d+\.))?\d+/
 
-				// calculate for higher pattern first
-				while(statement.search(higher_pattern) > -1){
-					let [result] = statement.match(higher_pattern);
-					let [,operand1, operator, operand2] = getOperandsAndOperator(result);
-					let calc_val = compute([operand1, operand2], operator);
-					statement = statement.replace(result,calc_val);
-				}
-
-				// then calculate for lower pattern
-				while(statement.search(lower_pattern) > -1){
-					let [result] = statement.match(lower_pattern);
-					let [,operand1, operator, operand2] = getOperandsAndOperator(result);
-					let calc_val = compute([operand1, operand2], operator);
-					statement = statement.replace(result,calc_val);
-				}
-
-				statement = isNotDivisibleByZero(statement);
-
-				return statement;
+			// calculate for higher pattern first
+			while(statement.search(higher_pattern) > -1){
+				let [result] = statement.match(higher_pattern);
+				let [,operand1, operator, operand2] = getOperandsAndOperator(result);
+				let calc_val = compute([operand1, operand2], operator);
+				statement = statement.replace(result,calc_val);
 			}
+
+			// then calculate for lower pattern
+			while(statement.search(lower_pattern) > -1){
+				let [result] = statement.match(lower_pattern);
+				let [,operand1, operator, operand2] = getOperandsAndOperator(result);
+				let calc_val = compute([operand1, operand2], operator);
+				statement = statement.replace(result,calc_val);
+			}
+
+			statement = isNotDivisibleByZero(statement);
+
+			return statement;
 		}	
 	}
 
@@ -137,29 +179,41 @@ $(document).ready(function(){
 		return statement;
 	}
 
-	let isDivideZeroNotExist = () => {
-		let total = $('#total').text().trim();
-
-		if(total != 'Not divisible by zero')
-			return true;
-		return false;
-	}
 	
 	let reset = () => {
 		$('#formula').empty();
 		$('#total').empty();
 
-		let theme = $('.toggle-btn > .active').data('theme');
 		$('.screen').css({'justify-content': '','font-size' : '1.3rem'});
 		$('#formula').css({'display': 'block'});
-		switch(theme){
-			case 1:
-				$('#total').css({'color': 'var(--theme1-total)'}); break;
-			case 2:
-				$('#total').css({'color': 'var(--theme2-total)'}); break;
-			case 3:
-				$('#total').css({'color': 'var(--theme3-total)'}); break;	
-		}
+		$('#total').removeClass('eql');
 	}
+
+	let isValidDot = (statement) => {
+		statement = statement + '.';
+
+		if(statement.search(/\.\.|\d+\.\d+\./) != -1)
+			return false;
+		return true;
+	}
+
+	let isDoubleOperator = (statement, op) => {
+		statement = statement + op;
+
+		if(statement.search(/[×\/\+-]{2,}/) > -1)
+			return true;
+		return false;
+
+	}
+
+	let isValidOperator = (statement, op) => {
+		statement = statement + op;
+
+		// if doesn't starts with +, /, x
+		if(statement.search(/^[^\+\/×]/) > -1)
+			return true;
+		return false;
+	}
+
 
 });
